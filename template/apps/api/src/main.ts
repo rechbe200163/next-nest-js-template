52f9723445e49dbd8bddf340a1ac115c6f0accef
Michael Julian Rechberger <rechberger.michael@aol.com>
2026-01-04 10:40:55 +0100

    [api] updated main.ts to use swagger config file

--- template/apps/api/src/main.ts
+++ template/apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { configureSwaggerUI } from 'config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,13 +10,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  const config = new DocumentBuilder()
-    .setTitle('Median')
-    .setDescription('The Median API description')
-    .setVersion('0.1')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  configureSwaggerUI(app);
 
   await app.listen(process.env.PORT || 3333);
 }
